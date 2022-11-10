774aa87cece4ffcf7b5bba8b6fa843a251b6b32c
Majdi-msallem <majdi.msallem@esprit.tn>
2022-11-10 22:00:18 +0100

    tests

--- src/test/java/com/esprit/examen/services/ProduitServiceImplMock.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplMock.java
@@ -1,8 +1,8 @@
 package com.esprit.examen.services;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
