46b6e3c2f28945a017d735506dc5ee34ccc86980
Majdi-msallem <majdi.msallem@esprit.tn>
2022-11-11 01:26:47 +0100

    try test2

---
+++ src/test/java/com/esprit/examen/services/TpAchatProjectApplicationTest.java
@@ -0,0 +1,17 @@
+package com.esprit.examen.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAchatProjectApplicationTest {
+
+	@Test
+	void contextLoads() {
+		int k = 4;
+		assertEquals(4, k);
+	}
+
+}
