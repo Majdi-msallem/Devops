2d1498b0c01086aaeb98b971d27d2ed8926a3b84
Majdi-msallem <majdi.msallem@esprit.tn>
2022-11-11 01:18:28 +0100

    try test

--- src/test/java/com/esprit/examen/services/ProduitServiceImplMockTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplMockTest.java
@@ -1,103 +1,64 @@
 package com.esprit.examen.services;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.jupiter.api.Test;
+
 import java.util.Optional;
-import com.esprit.examen.entities.CategorieProduit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import com.esprit.examen.entities.Produit;
-import com.esprit.examen.repositories.CategorieProduitRepository;
 import com.esprit.examen.repositories.ProduitRepository;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class ProduitServiceImplMockTest {
 
-	
-	@Mock
-	ProduitRepository prodRepo ; 
-	
-	@Mock
-	CategorieProduitRepository catRepo ; 
-	
-	@InjectMocks
-	ProduitServiceImpl prodServ;
-	
-	@InjectMocks
-	CategorieProduitServiceImpl catServ;
-	
-	Produit p = new Produit("Code","Libelle");
-	
-	List<Produit> list = new ArrayList<Produit> () {
-		{
-			add(new Produit("Code","Libelle"));
-			add(new Produit("Code","Libelle"));
-			
-		}
-	};
-	
-	@Test  
-	public void testRetrieveAllProduits() {
-		Mockito.when(prodRepo.findAll()).thenReturn(list);
-		assertEquals(list.size(), prodServ.retrieveAllProduits().size());
-	}
+	  @InjectMocks
+	    ProduitServiceImpl  produitService ;
 
-	
-	@Test 
-	public void testAddProduit() {
-		CategorieProduit cat=new CategorieProduit("cat1", "cat2"); 
-		Mockito.when(prodRepo.save(any(Produit.class))).thenReturn(p);
-		Mockito.when(catRepo.save(any(CategorieProduit.class))).thenReturn(cat);
-		CategorieProduit cat1 = catServ.addCategorieProduit(cat);
-		Produit f1 = prodServ.addProduit(p);
-		assertNotNull(f1);
-		assertTrue(p.getCodeProduit().length() > 0);
-		
-	}
-	
-	@Test 
-	public void testRetrieveProduit() {
-		
-		Mockito.when(prodRepo.findById(Mockito.anyLong())).thenReturn(Optional.of(p) );	
-		Produit f = prodServ.retrieveProduit(2L);
-		assertNotNull(f);
-		
-	}
-	
-	@Test 
-	public void testUpdateProduit() {
-		Produit p = new Produit("CODE","LIBELEL");
-		System.out.println("++++++PRODUIT++++++++++"+p);
-		Mockito.when(prodRepo.save(any(Produit.class))).thenReturn(p);	
-		p.setCodeProduit("CodeProduit");
-		p.setLibelleProduit("LibelleProduit");
-		System.out.println("++++UPDATE++++"+p);
-		Produit pUpdated = prodServ.updateProduit(p);
-		assertNotNull(pUpdated);
-		assertEquals(p.getLibelleProduit(),pUpdated.getLibelleProduit());
+	    @Mock
+	    ProduitRepository produitRepository ;
 
-	}
-	
-	@Test 
-	public void testDeleteProduit() {
-		Produit p = new Produit("CODE","LIBELEL");
-		p.setIdProduit(1L);	
-		doNothing().when(prodRepo).deleteById(Mockito.anyLong());
-		prodServ.deleteProduit(1L);
-		verify(prodRepo,times(1)).deleteById(p.getIdProduit());
-	}
+	    @Test
+	    public void testGetallProducts(){
+	        when(produitRepository. findAll()).thenReturn(Stream
+	                .of(new Produit(), new Produit()).collect(Collectors.toList()));
+	        assertEquals(2, produitService.retrieveAllProduits().size());
+	    }
+
+	    @Test
+	    public void addProduitTest(){
+	        Produit prod = new Produit();
+	        when(produitRepository.save(prod)).thenReturn(prod);
+	        assertEquals(prod, produitService.addProduit(prod));
+	    }
+
+	    @Test
+	    public void deleteUserTest(){
+	        Produit prod = new Produit();
+	        produitService.deleteProduit(prod.getIdProduit());
+	        verify(produitRepository, times(1)).deleteById(prod.getIdProduit());
+	    }
+
+	    @Test
+	    public void testUpdateProduct(){
+	        Produit prod = new Produit();
+	        when(produitRepository.save(prod)).thenReturn(prod);
+	        assertEquals(prod, produitService.updateProduit(prod));
+	    }
+
+	    @Test
+	    public void testGetProduct(){
+	        Produit prod = new Produit();
+	        when(produitRepository.findById(prod.getIdProduit())).thenReturn(Optional.of(prod));
+	        assertEquals(prod, produitService.retrieveProduit(prod.getIdProduit()));
+	    }
+
 }
